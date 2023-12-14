c9534eaf7a6c3c39e93dd3e15a9f7eed7e5fdeab
Isaias Julio <isaiasjulio@Isaiass-MacBook-Air-2.local>
2023-12-13 21:52:58 -0500

    arrayRendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css'
+import {puppyList} from '../src/data.js'
+
+import { useState } from 'react'
 
 function App() {
+const [puppies, setPuppies] = useState(puppyList)
+console.log("puppyList:", puppyList);
   return (
-    <>
       <div>
-        
+        {
+          puppies.map((puppy) => {
+          return <p key ={puppy.id}>{puppy.name}</p>;
+          })
+        }
       </div>
-    </>
-  )
+      // a key is a special attribute you need to inlcude
+      // when creating a list of elements in React
+  );
 }
 
 export default App
