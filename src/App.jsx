62e66ca97856af0bda44912c4f233278160dc3ee
Isaias Julio <isaiasjulio@Isaiass-MacBook-Air-2.local>
2023-12-14 18:19:37 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
-import {puppyList} from '../src/data.js'
-
-import { useState } from 'react'
+import { puppyList } from '../src/data.js';
+import { useState } from 'react';
+import '../src/App.css'
 
 function App() {
-const [puppies, setPuppies] = useState(puppyList)
-console.log("puppyList:", puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  // Step 1: Create a variable named featuredPup
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  // Step 2: Log featuredPup
+  console.log('Featured Puppy:', featuredPup);
+
+  // Step 3: Click handler to set featPupId
+  const handleClick = (puppyId) => {
+    setFeatPupId(puppyId);
+  };
+
   return (
-      <div>
-        {
-          puppies.map((puppy) => {
-          return <p key ={puppy.id}>{puppy.name}</p>;
-          })
-        }
-      </div>
-      // a key is a special attribute you need to inlcude
-      // when creating a list of elements in React
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+
+      {/* Step 4: Conditionally render p tag if featPupId is not null */}
+      {featPupId && <p>{featPupId}</p>}
+
+      {/* Step 5: Render Puppy Data */}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 }
 
-export default App
+export default App;
